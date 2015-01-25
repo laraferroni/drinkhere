9076fc06a7c607347b790676dc25a815892fcfca
Lara Ferroni <lara@laraferroni.com>
2015-01-24 17:17:42 -0800

    auto add settings and user for new accounts

--- db/migrate/20150120183837_add_settings.rb
+++ db/migrate/20150120183837_add_settings.rb
@@ -1,13 +1,5 @@
 class AddSettings < ActiveRecord::Migration
   def change
-  	create_table :settings do |t|
-      t.integer :account_id
-      t.string :url
-      t.timestamps
-    end
-
-    add_attachment :achievements, :homepageimage
-    add_column :achievements, :prereq_id, :integer
-
+  	#moved to add_accounts migration 
   end
 end
