8a549c6696027f6048fb8e35f6cbc39144755717
Lara Ferroni <lara@laraferroni.com>
2015-02-01 14:28:52 -0800

    got tired of asset path problems with heroku and paperclip. moved default image urls to s3

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,8 +1,8 @@
 class Achievement < ActiveRecord::Base
   include Rails.application.routes.url_helpers
 
-  has_attached_file :homepageimage, :styles => {:large => "1200x1200", :medium => "500x500>", :thumb => "100x100>" }, :default_url => ActionController::Base.helpers.asset_path("missing_homepage_image.jpg")
-  has_attached_file :icon, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => ActionController::Base.helpers.asset_path("missing.png")
+  has_attached_file :homepageimage, :styles => {:large => "1200x1200", :medium => "500x500>", :thumb => "100x100>" }, :default_url => "https://s3-us-west-2.amazonaws.com/drinkhereassets/images/missing_homepage_image.jpg"
+  has_attached_file :icon, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => "https://s3-us-west-2.amazonaws.com/drinkhereassets/images/missing.png"
   validates_attachment_content_type :homepageimage, :content_type => /\Aimage\/.*\Z/
   validates_attachment_content_type :icon, :content_type => /\Aimage\/.*\Z/
 	acts_as_tenant(:account)
