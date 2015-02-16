140c4d8626cf0b98f4079571b8750fbbd9e5020a
Cam Ferroni <cam@ferroni.net>
2015-02-16 10:06:53 -0800

    Move paperclip S3 to production only

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set PaperClip options for production
+  config.paperclip_defaults config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_host_name => 's3-us-west-2.amazonaws.com',
+    :s3_protocol => 'https',
+    :s3_credentials => {
+      :bucket => ENV['AWS_S3_ASSETS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
 end
